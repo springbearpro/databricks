412f8402f753ba4b046e6daa46328eeca7d1cfd0
Fei <60535586+springbearpro@users.noreply.github.com>
2022-08-29 16:24:31 +0800

    Update nb.py

--- nb.py
+++ nb.py
@@ -1,6 +1,9 @@
 # Databricks notebook source
-print("notebook created from databricks")
 
+print("notebook created from databricks")
+df = spark.read.parquet("s3://dp-ue1-gen-galaxyp13n-aegis-dev/demo/seed_output/")
+df.show(100, False)
+print("spark show is done")
 # COMMAND ----------
 
 
